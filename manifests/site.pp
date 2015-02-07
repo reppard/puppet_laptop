8c12f66585fb4108903a1e63085e276304910534
reppard <reppardwalker@gmail.com>
2015-02-07 14:27:46 -0500

    feat(site.pp/Puppetfile): add vagrant"

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,24 +2,23 @@ node default {
   include people::reppard
   include golang
 
-  class { 'packer': install_dir => '/usr/local/bin', version => '0.7.5' }
-  class { 'rbenv': latest => true }
-  rbenv::plugin { 'sstephenson/ruby-build': }
-  rbenv::build { '2.1.2': global => true }
-
+  class { 'packer':  install_dir => '/usr/local/bin', version => '0.7.5' }
+  class { 'rbenv':   latest => true }
+  class { 'vagrant': }
+  class { 'nodejs': }
   class { 'docker':
     tcp_bind    => 'tcp://0.0.0.0:4243',
     socket_bind => 'unix:///var/run/docker.sock',
   }
 
-  class { 'nodejs': }
+  rbenv::plugin { 'sstephenson/ruby-build': }
+  rbenv::build { '2.1.2': global => true }
 
   package { 'steroids':
     ensure   => absent,
     provider => 'npm',
     require  => Class['nodejs'],
   }
-
   package {
     [
       'ack-grep',
