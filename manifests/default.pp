5d49c1ed89060ca74f56a4f25da2b0afbed48e91
reppard <reppardwalker@gmail.com>
2015-09-20 12:21:17 -0400

    feat(*): java and such for fedora/centos

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,30 @@
 node default {
-  class { 'people': user   => reppard }
-  class { 'rbenv':  latest => true }
+  include vault
+
+  class { 'people': user    => reppard }
+  class { 'rbenv':  latest  => true }
   class { 'vagrant': }
+
   class { 'nodejs':
     version      => 'v0.12.2',
     make_install => false,
   }
+
   class { 'docker':
     tcp_bind    => 'tcp://0.0.0.0:4243',
     socket_bind => 'unix:///var/run/docker.sock',
+  }
+
+  class { 'jdk8':
+    notify => Exec['install_alternative'],
+  }
+
+  exec { 'install_alternative':
+    command => '/usr/sbin/alternatives --install /usr/bin/java java /usr/java/default/bin/java 1',
+  }
+  exec { 'update_alternatives':
+    command =>  '/usr/sbin/update-alternatives --set java /usr/java/default/bin/java',
+    require => Exec['install_alternative'],
   }
 
   rbenv::plugin { 'sstephenson/ruby-build': }
@@ -16,12 +32,14 @@ node default {
 
   package {
     [
-      'ack-grep',
+      'ack',
       'findutils',
-      'tar',
-      'tmux',
+      'gimp',
+      'htop',
       'openssh-server',
-      'tig'
+      'tar',
+      'tig',
+      'tmux',
     ]:
   }
 }
