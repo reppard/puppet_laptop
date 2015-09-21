60edeb75e4032e9e3dc1be59a60b94f1a3b71fee
reppard <reppardwalker@gmail.com>
2015-09-21 18:04:27 -0400

    feat(bootstrap_centos): add rpmfusion to bootstrap script

--- bootstrap_centos.sh
+++ bootstrap_centos.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 sudo yum update -y
+
+sudo yum install -y \
+  --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 sudo yum install -y puppet ruby ruby-devel wget unzip git
+
 sudo gem install --bindir /usr/bin --no-ri --no-rdoc librarian-puppet
+
 librarian-puppet install
-sudo puppet apply --hiera_config hiera.yaml --verbose --debug --modulepath=modules manifests/default.pp
+
+sudo puppet apply \
+  --hiera_config hiera.yaml \
+  --verbose --debug \
+  --modulepath=modules manifests/default.pp
